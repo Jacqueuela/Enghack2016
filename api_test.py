238e5e9cb9e920ac337208878753e511672ff798
Leo Kim <lkim@v1020-wn-241-228.campus-dynamic.uwaterloo.ca>
2016-06-26 03:11:56 -0400

    Cleaned up some code, added comments, and made things more "Pythonic"

--- api_test.py
+++ api_test.py
@@ -1,10 +1,6 @@
 from urllib2 import Request, urlopen
 import json
-
-Movies = {}
-#weighted_movies = []
-checked = []
-call_count = 0
+import time
 
 class Movie:
     def __init__(self, raw_movie):
@@ -16,62 +12,86 @@ class Movie:
         self.popularity = raw_movie['popularity']
         self.count = raw_movie['vote_count']
         self.weight = 0
-    @property
-    def score(self):
-        return 0.7 * self.weight / call_count + 0.2 * self.rating / 10 + 0.1 * self.popularity / 100
 
+    def update_score(self, count):
+        self.score = 0.7 * self.weight / count + 0.2 * self.rating / 10 + 0.1 * self.popularity / 100
 
-# Acquire a list of movies based off of genre
-def generate_movies(genre, page):
-    request = Request('http://api.themoviedb.org/3/genre/' + genre + '/movies?api_key=03bb4843dcfd206b47dc2872f71aa418&page=' + str(page))
-    response_body = urlopen(request).read()
-    return json.loads(response_body)
+class Recommender:
+    def __init__(self):
+        self.Movies = {}
+        self.checked = []
+        self.call_count = 0
+    # Acquire a list of movies based off of genre
+    @staticmethod
+    def generate_movies(genre, page):
+        request = Request('http://api.themoviedb.org/3/genre/' + genre + '/movies?api_key=03bb4843dcfd206b47dc2872f71aa418&page=' + str(page))
+        response_body = urlopen(request).read()
+        return json.loads(response_body)
 
-# Adds total list of Movies
-def add_movies(raw_movies):
-    for movie_index in raw_movies['results']:
-        if movie_index['id'] not in Movies:
-            Movies[movie_index['id']] = Movie(movie_index)
+    # Adds total list of Movies
+    def add_movies(self, raw_movies):
+        for movie_index in raw_movies['results']:
+            if movie_index['id'] not in self.Movies:
+                self.Movies[movie_index['id']] = Movie(movie_index)
 
-# Updates the total list of Movies
-def update_movies(genre):
-    for i in range(5):
-        add_movies(generate_movies(genre, i+1))
-    checked.append(genre)
+    # Updates the total list of Movies
+    def update_movies(self, genre):
+        for i in range(5):
+            self.add_movies(self.generate_movies(genre, i+1))
+        self.checked.append(genre)
 
-def weigh(movie, genre_first, genre_second):
-    if genre_first in movie.genre:
-        movie.weight += 1.
-    elif genre_second in movie.genre:
-        movie.weight += 0.5
+    # Weighs the movies by genre based on user's input
+    @staticmethod
+    def weigh(movie, genre_first, genre_second):
+        if genre_first in movie.genre:
+            movie.weight += 1.
+        elif genre_second in movie.genre:
+            movie.weight += 0.5
 
-def list_from_dict(dict):
-    arr = []
-    for key, movie in dict.iteritems():
-        arr.append(movie)
-    return arr
+    # Makes a list of objects from an input dictionary
+    @staticmethod
+    def list_from_dict(movie_map):
+        arr = []
+        for key, movie in movie_map.iteritems():
+            arr.append(movie)
+        return arr
 
-def new_entry(genre_first, genre_second):
-    if genre_first not in checked:
-        update_movies(str(genre_first))
-    if genre_second not in checked:
-        update_movies(str(genre_second))
-    for key, movie in Movies.iteritems():
-        weigh(movie, genre_first, genre_second)
-    global call_count
-    call_count += 1
+    # New genre entry
+    def new_entry(self, genre_first, genre_second):
+        if genre_first not in self.checked:
+            self.update_movies(str(genre_first))
+            time.sleep(2)
+        if genre_second not in self.checked:
+            self.update_movies(str(genre_second))
+            time.sleep(2)
+        for key, movie in self.Movies.iteritems():
+            self.weigh(movie, genre_first, genre_second)
+        self.call_count += 1
 
-def read_input(arr):
-    for dikt in arr:
-        new_entry(dikt['genres'][0], dikt['genres'][1])
+    # Communicates with the flask files
+    def read_input(self, arr):
+        for dikt in arr:
+            self.new_entry(dikt['genres'][0], dikt['genres'][1])
 
-def final():
-    arr_movies = list_from_dict(Movies)
-    sorted_movies = sorted(arr_movies, key=lambda movie: movie.score, reverse=True)
-    for movie in sorted_movies[0:30]:
-        print(movie.title)
-        print(movie.score)
+    # Final call from host, returns an array of sorted movies by score
+    def final(self):
+        arr_movies = self.list_from_dict(self.Movies)
+        for movie in arr_movies:
+            movie.update_score(self.call_count)
+        sorted_movies = sorted(arr_movies, key=lambda movie: movie.score, reverse=True)
+        return sorted_movies
+        # for movie in sorted_movies[0:30]:
+        #     print(movie.title)
+        #     print(movie.score)
 
-new_entry(27, 28)
-new_entry(18, 27)
-final()
+# rc= Recommender()
+# rc.new_entry(27, 28)
+# rc.new_entry(18, 14)
+# rc.new_entry(12, 18)
+# rc.new_entry(10751, 14)
+# rc.new_entry(28, 35)
+# rc.new_entry(18, 28)
+# rc.new_entry(18, 28)
+# rc.new_entry(18, 28)
+# rc.new_entry(18, 28)
+# rc.final()
