5ba7795ae5d3b88c80ec828aa0999a1db75997cf
Sakib Haider <shaider@v1020-wn-254-53.campus-dynamic.uwaterloo.ca>
2016-06-26 05:31:28 -0400

    functional commit

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask.ext.api.decorators import set_parsers
 from flask import request
 from flask.ext.api.parsers import JSONParser
 from GenreMap import Genre_Map
-#import api_test
+from api_test import Recommender
+from api_test import Movie
 app = Flask(__name__)
 #Get JSON from Front end
 #@app.route("""Form""", methods['GET'])
@@ -12,41 +13,43 @@ app = Flask(__name__)
 # @set_parsers(JSONParser)
 # def form_input():
 # 	return {'request data':request.data}
+rc = Recommender()
 genre_list = Genre_Map()
-store_datum = []
+
 genres = []
 Genre = []
+final_list = []
+a = {}
 def format(dinosaur): #Error checking done here as well
-	# if """Not 2 genres error out""":
-	# 	"""Error out"""	pass
-	# # else if """length value isn't at least 1 hour""":
-	# # 	"""Error out""" pass
-	# else:
-	store_datum.append(dinosaur)
+	
+
 
 	genres.append(dinosaur['genre1']) 
 	genres.append(dinosaur['genre2'])
 	
 	Genre = [genre_list.map[genres[0]], genre_list.map[genres[1]]]
-	print Genre 
 	return Genre
 
 @app.route("/Store", methods=['POST'])
 def store_data():
 	
+	#final_list.append(format(request.json))
 	
-	format(request.json)
-	
-	return jsonify({"Response" : "Entry Added"})
-
-
+	return jsonify({"Response" : "Entry Added"})	
 
 """Waits until Final request Before sending"""
 
 @app.route("/Suggestions", methods=['GET'])
 def find_suggestions():
+	index = 0
 	#Pass in the array to Leo
-	return jsonify(Suggestions)
+	final_list.append(format(request.json))
+	for user_input in final_list:
+		rc.new_entry(user_input[0], user_input[1])
+	for movie in rc.final():
+		a[index] = {'title':movie.title, 'overview':movie.overview, 'rating':movie.rating}
+		index+=1
+	return jsonify(a)
 	
 # @app.route("""/Output page""", methods=['POST'])
 # def give_suggestions():
